f4511c51152c06dbe2114b5ef5462ab02d7f100f
LeeeGeonJae <wnwkrdkemf@gmail.com>
2022-01-18 17:08:02 +0900

    Image 클래스 만들기

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include "Transition.h"
 #include "State.h"
+#include "Image.h"
 #include <vector>
 
 using namespace std;
 
 int main()
 {
+
 	vector<State*> StateList;
 	StateList.push_back(new State(1, "배회"));
 	StateList.push_back(new State(2, "추격"));
@@ -40,6 +42,47 @@ int main()
 			cout << StateList[i]->StateName << endl;
 		}
 	}
+
+
+	class Image
+	{
+	public:
+		Image();
+		Image(int NewX, int NewY, int NewR, int NewG, int NewB)
+		{
+			X = NewX;
+			Y = NewY;
+			R = NewR;
+			G = NewG;
+			B = NewB;
+		}
+
+		~Image();
+
+		int X;
+		int Y;
+		int R;
+		int G;
+		int B;
+
+	};
+
+
+
+	vector<Image*> ImageList;
+	ImageList.push_back(new Image(0, 0, 165, 55, 128));
+	ImageList.push_back(new Image(0, 1, 133, 28, 182));
+	ImageList.push_back(new Image(0, 2, 115, 136, 63));
+	ImageList.push_back(new Image(1, 0, 209, 132, 221));
+	ImageList.push_back(new Image(1, 1, 123, 72, 106));
+	ImageList.push_back(new Image(1, 2, 105, 117, 92));
+	ImageList.push_back(new Image(2, 0, 10, 98, 237));
+	ImageList.push_back(new Image(2, 1, 134, 222, 36));
+	ImageList.push_back(new Image(2, 2, 217, 8, 171));
+	ImageList.push_back(new Image(3, 0, 223, 163, 22));
+	ImageList.push_back(new Image(3, 1, 64, 10, 131));
+	ImageList.push_back(new Image(3, 2, 220, 111, 106));
+
 
 
 
